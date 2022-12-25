bfdca27bbaf06b920e7b788a12c3dfd702ee8ef8
BetterCallSlade <BetterCallSlade@pm.me>
2022-12-25 16:06:42 +0800

    邻接矩阵、邻接表

---
+++ 图/adjacency_list_graph.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+	int val;
+	struct node* next;
+} node;
+
+#define VERTICES 5
+
+typedef struct {
+	int edges;
+	node* adjacency_list[VERTICES];
+} Graph;
+
+node* CreateNode(int val)
+{
+	node* new_node = malloc(sizeof(node));
+	new_node->val = val;
+	new_node->next = NULL;
+	return new_node;
+}
+
+Graph* CreateGraph()
+{
+	Graph* graph = malloc(sizeof(Graph));
+	scanf_s("%d", &graph->edges);
+	for (int i = 0; i < VERTICES; ++i)
+	{
+		graph->adjacency_list[i] = NULL;
+	}
+	int va, vb;
+	for (int i = 0; i < graph->edges; ++i)
+	{
+		scanf_s("%d", &va);
+		scanf_s("%d", &vb);
+		if (graph->adjacency_list[va] == NULL)
+		{
+			graph->adjacency_list[va] = CreateNode(vb);
+		}
+		else
+		{
+			node* node = graph->adjacency_list[va];
+			while (node->next != NULL)
+			{
+				node = node->next;
+			}
+			node->next = CreateNode(vb);
+		}
+		if (graph->adjacency_list[vb] == NULL)
+		{
+			graph->adjacency_list[vb] = CreateNode(va);
+		}
+		else
+		{
+			node* node = graph->adjacency_list[vb];
+			while (node->next != NULL)
+			{
+				node = node->next;
+			}
+			node->next = CreateNode(va);
+		}
+	}
+	return graph;
+}
+
+void PrintGraph(Graph* graph)
+{
+	for (int i = 0; i < VERTICES; ++i)
+	{
+		node* node = graph->adjacency_list[i];
+		while (node != NULL)
+		{
+			printf("%d ", node->val);
+			node = node->next;
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	Graph* graph = CreateGraph();
+	PrintGraph(graph);
+}
---
+++ 图/adjacency_matrix_graph.c
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	int vertices;
+	int edges;
+	int** adjacency_matrix;
+} Graph;
+
+Graph* CreateGraph()
+{
+	Graph* graph = malloc(sizeof(Graph));
+	scanf_s("%d", &graph->vertices);
+	graph->adjacency_matrix = malloc(sizeof(int*) * graph->vertices);
+	for (int i = 0; i < graph->vertices; ++i)
+	{
+		graph->adjacency_matrix[i] = malloc(sizeof(int) * graph->vertices);
+		for (int j = 0; j < graph->vertices; ++j)
+		{
+			graph->adjacency_matrix[i][j] = 0;
+		}
+	}
+	scanf_s("%d", &graph->edges);
+	int va, vb;
+	for (int i = 0; i < graph->edges; ++i)
+	{
+		scanf_s("%d", &va);
+		scanf_s("%d", &vb);
+		graph->adjacency_matrix[va][vb] = 1;
+		graph->adjacency_matrix[vb][va] = 1;
+	}
+	return graph;
+}
+
+void PrintGraph(Graph* graph)
+{
+	printf("vertices = %d，edges = %d\n", graph->vertices, graph->edges);
+	for (int i = 0; i < graph->vertices; ++i)
+	{
+		for (int j = 0; j < graph->vertices; ++j)
+		{
+			printf("%d ", graph->adjacency_matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	Graph* graph = CreateGraph();
+	PrintGraph(graph);
+}
